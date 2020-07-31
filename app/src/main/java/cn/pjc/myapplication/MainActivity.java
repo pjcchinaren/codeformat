0afc475279507b1f44ef1554b89b5962b7899f51
peng_jin_cheng <peng_jin_cheng@163.com>
2020-07-31 17:31:21 +0800

    window 10 commit

--- app/src/main/java/cn/pjc/myapplication/MainActivity.java
+++ app/src/main/java/cn/pjc/myapplication/MainActivity.java
@@ -1,9 +1,10 @@
 package cn.pjc.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         findViewById(R.id.tv);
 
     }
@@ -19,22 +21,27 @@ public class MainActivity extends AppCompatActivity {
      * @param i
      */
     private void method1(int i) {
-
-
-
-
-
-
+        //dfsaf
+        //
         Toast.makeText(this, "hahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahah", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this,"hehe",Toast.LENGTH_SHORT)
-                .show();
-        Toast.makeText(this,"hhe",Toast.LENGTH_SHORT).show();
+//adfk
+
+        Toast.makeText(this, "hehe", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hhe", Toast.LENGTH_SHORT).show();
+
+
+        // fsdajflk
+    }
+
+    private void a() {
+
+
     }
 
     private void method2() {
 
-
         Log.e("aaa", "1");
+
         Log.e("aaa", "1");
         Log.e("aaa", "1");
     }
