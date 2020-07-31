e3debc8bc9aa9f576a03f84752d9deda44e556e7
jianghy <505044110@qq.com>
2020-07-31 10:41:20 +0800

    mac first commit

--- app/src/main/java/cn/pjc/myapplication/MainActivity.java
+++ app/src/main/java/cn/pjc/myapplication/MainActivity.java
@@ -11,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.tv);
+
+    }
+
+    /**
+     *
+     * @param i
+     */
+    private void method1(ini i) {
+        android.widget.Toast.makeText(this, "nihao", Toast.LENGTH_SHORT).show();
+        android.widget.Toast.makeText(this, "nihao", Toast.LENGTH_SHORT).show();
+        android.widget.Toast.makeText(this, "nihao", Toast.LENGTH_SHORT).show();
     }
 }
