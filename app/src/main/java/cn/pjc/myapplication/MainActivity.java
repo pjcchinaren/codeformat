83fefcdf9465300156e76a39001168bf4911b582
jianghy <505044110@qq.com>
2020-07-31 11:11:38 +0800

    mac 9 commit

--- app/src/main/java/cn/pjc/myapplication/MainActivity.java
+++ app/src/main/java/cn/pjc/myapplication/MainActivity.java
@@ -19,12 +19,21 @@ public class MainActivity extends AppCompatActivity {
      * @param i
      */
     private void method1(int i) {
+
+
+
+
+
+
         Toast.makeText(this, "hahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahahhahahahahah", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this,"hehe",Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "hhe", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"hehe",Toast.LENGTH_SHORT)
+                .show();
+        Toast.makeText(this,"hhe",Toast.LENGTH_SHORT).show();
     }
 
     private void method2() {
+
+
         Log.e("aaa", "1");
         Log.e("aaa", "1");
         Log.e("aaa", "1");
